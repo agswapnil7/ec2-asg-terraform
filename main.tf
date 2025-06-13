3d2a2a44c1b443c5b52de183805a3c8041bbd71a
Swapnil Neupane <102732227+agswapnil7@users.noreply.github.com>
2025-06-13 14:49:04 +0545

    Update main.tf  ami ID

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_launch_template" "web" {
   name_prefix   = "web-lt-"
-  image_id      = "ami-0c02fb55956c7d316"  # Amazon Linux 2
+  image_id      = "ami-09e6f87a47903347c"  # Amazon Linux 2
   instance_type = "t2.micro"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
